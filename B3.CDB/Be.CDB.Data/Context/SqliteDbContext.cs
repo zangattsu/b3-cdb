37e3fe76bc38080bde21f414753a489f6fc8321d
Claudio Santana <claudio.a.santana@outlook.com>
2025-02-19 23:54:42 -0300

    Criado o projeto de dados.

---
+++ B3.CDB/Be.CDB.Data/Context/SqliteDbContext.cs
@@ -0,0 +1,34 @@
+﻿using B3.CDB.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Be.CDB.Data.Context
+{
+    public class SqliteDbContext : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("FileName=sqlite_cdb", option =>
+            {
+                option.MigrationsAssembly(Assembly.GetExecutingAssembly().FullName);
+            });
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Imposto>()
+                .ToTable("Impostos", "teste")
+                .HasData(new Imposto { Id = 1, AteQueMes = 6, Descricao = "CDB 1 Description", Percentual = 12 });
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
